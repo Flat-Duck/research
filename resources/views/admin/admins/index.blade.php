1d4d7476bccac1101474483d805832e6eb287d12
Flat-Duck <xile.meko@gmail.com>
2020-12-20 02:28:30 +0200

    add retrict login for non active admins

--- resources/views/admin/admins/index.blade.php
+++ resources/views/admin/admins/index.blade.php
@@ -39,7 +39,7 @@
                 @endif 
                       
                             <td>
-                                @if ($admin->id==auth()->guard('admin')->user()->id || $admin->id==1)
+                                @if ($admin->id==auth()->guard('admin')->user()->id ||   $admin->id==1)
                                  
                                     @else
                                  
